dd7a8fddec48c71192a0ee3d37ed9cfc5c991622
Rahul Shaw <103486958+myselfrahul6290@users.noreply.github.com>
2023-03-15 22:53:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,43 +33,43 @@ var item=[
     {
         'id':0,
         'name':"Pizza",
-        'src':"../img/p1.jpg" ,
+        'src':"img/p1.jpg" ,
         'price':150
     },
     {
         'id':1,
         'name':"Burger",
-        'src':"../img/p7.jfif" ,
+        'src':"img/p7.jfif" ,
         'price':400
     },
     {
         'id':2,
         'name':"momo",
-        'src':"../img/p4.jpg" ,
+        'src':"img/p4.jpg" ,
         'price':300
     },
     {
         'id':3,
         'name':"Chicken Biryani",
-        'src':"../img/p9.jpg" ,
+        'src':"img/p9.jpg" ,
         'price':350
     },
     {
         'id':4,
         'name':"mutton curry",
-        'src':"../img/p10.webp" ,
+        'src':"img/p10.webp" ,
         'price':650
     },
     {
         'id':5,
         'name':"Fish fry",
-        'src':"../img/p11.jfif" ,
+        'src':"img/p11.jfif" ,
         'price':450
     },
     {
         'id':6,
         'name':"Noodles",
-        'src':"../img/p6.jpg" ,
+        'src':"img/p6.jpg" ,
         'price':350
     }
 
